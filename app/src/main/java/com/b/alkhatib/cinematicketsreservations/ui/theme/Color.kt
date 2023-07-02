ce8d124865d32c5b9c2490dc1ad9fb24c01be9a9
Bilal Alkhatib <bilaloalkhatib.02@gmail.com>
2023-07-02 18:54:16 +0300

    add all design material

---
+++ app/src/main/java/com/b/alkhatib/cinematicketsreservations/ui/theme/Color.kt
@@ -0,0 +1,28 @@
+package com.b.alkhatib.cinematicketsreservations.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Orange = Color(0xFFFD5524)
+val layoutBackground = Color(0xFFFEFEFE)
+
+val PrimaryTextColor = Color(0xFF0C1011)
+val SecondaryTextColor = Color(0xFF2B2D2C)
+val ThirdTextColor = Color(0xFF7A7A7A)
+
+val SelectedCardColor = Color(0xFF948B7E)
+val BorderCardColor = Color(0xFFEDEDED)
+
+val Gray = Color(0xFF404040)
+
+val CancelButtonBackground = Color(0xFF9E674C)
+
+
+
---
+++ app/src/main/java/com/b/alkhatib/cinematicketsreservations/ui/theme/Font.kt
@@ -0,0 +1,11 @@
+package com.b.alkhatib.cinematicketsreservations.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.b.alkhatib.cinematicketsreservations.R
+
+val Product = FontFamily(
+    Font(R.font.product_sans_regular, weight = FontWeight.Normal),
+    Font(R.font.product_sans_medium, weight = FontWeight.Medium)
+)
